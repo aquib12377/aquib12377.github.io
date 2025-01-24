b17c256f6f7e5ff1e994fa8989cdeb2ac0049234
Mohammed Aquib Ansari <75412827+aquib12377@users.noreply.github.com>
2025-01-25 01:30:49 +0530

    Update sw.js

--- Web/sw.js
+++ Web/sw.js
@@ -5,8 +5,8 @@ const urlsToCache = [
   "/inventory.html",
   "/user.html",
   "/manifest.json",
-  "/Web/icons/icon-192x192.png",
-  "/Web/icons/icon-512x512.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
 ];
